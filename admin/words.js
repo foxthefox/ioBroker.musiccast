40ca62d0df1f04507a13e34d5b3f68e6663a8ee8
foxthefox <github@wysiwis.net>
2017-04-30 23:05:24 +0200

    Update words.js

--- admin/words.js
+++ admin/words.js
@@ -1,6 +1,6 @@
 // Dictionary (systemDictionary is global variable from adapter-settings.js)
 systemDictionary = {
-	"musiccast adapter settings": {"de": "Beispiel", "ru": ""},
+	"musiccast adapter settings": {"de": "Einstellungen für MusicCast Adapter", "ru": ""},
 	"on save adapter restarts with new config immediately": {
 		"de": "Beim Speichern von Einstellungen der Adapter wird sofort neu gestartet.",
 		"ru": "??"
