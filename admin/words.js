7e03a1bda1c27a2b9ab7ce2caad74563bfc5ac49
foxthefox <github@wysiwis.net>
2017-05-23 19:55:28 +0200

    Update words.js

--- admin/words.js
+++ admin/words.js
@@ -1,8 +1,34 @@
 // Dictionary (systemDictionary is global variable from adapter-settings.js)
 systemDictionary = {
-	"musiccast adapter settings": {"de": "Einstellungen für MusicCast Adapter", "ru": ""},
+	"Yamaha MusicCast adapter": {
+		"de": "Yamaha MusicCast Adapter", 
+		"ru": ""},
+	"Yamaha MusicCast settings": {
+		"de": "Yamaha MusicCast Einstellungen", 
+		"ru": ""},
 	"on save adapter restarts with new config immediately": {
 		"de": "Beim Speichern von Einstellungen der Adapter wird sofort neu gestartet.",
-		"ru": "??"
-	}
+		"ru": ""},
+	"Yamaha MusicCast devices": {
+		"de": "Yamaha MusicCast Geräte",
+		"ru": ""},	
+	"Try to push the Search Button until you find the amount of MusicCast devices in your home.": {
+		"de": "Drücke Search sooft, bis alle MusicCast geräte gefunden sind.",
+		"ru": ""},
+	"Use "+" for manual configuration.": {
+		"de": "Benutze + für manuelle Konfiguration",
+		"ru": ""},
+	"IP address": {
+		"de": "IP Adresse",
+		"ru": ""},	
+	"model": {
+		"de": "Modell",
+		"ru": ""},
+	"uid": {
+		"en": "DeviceID
+		"de": "GeräteID",
+		"ru": ""},
+	"Name": {
+		"de": "Bezeichnung",
+		"ru": ""}			
 };
